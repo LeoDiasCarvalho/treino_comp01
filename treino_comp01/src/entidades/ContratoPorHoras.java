ce96af61ac9c99cda024e4d544102deaeb51ac8e
leo_dias <leo_dias@DESKTOP-FE887VA>
2021-08-11 20:26:59 -0300

    Criando classe Contrato por horas

---
+++ treino_comp01/src/entidades/ContratoPorHoras.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package entidades;
+
+import java.util.Date;
+
+/**
+ * @author leo_dias
+ *
+ */
+public class ContratoPorHoras {
+
+	private Date data;
+	private double valorPorHoras;
+	private int horas;
+	
+	public ContratoPorHoras() {
+	}
+	
+	public ContratoPorHoras(Date data, double valorPorHoras, int horas) {
+		this.data = data;
+		this.valorPorHoras = valorPorHoras;
+		this.horas = horas;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public double getValorPorHoras() {
+		return valorPorHoras;
+	}
+
+	public void setValorPorHoras(double valorPorHoras) {
+		this.valorPorHoras = valorPorHoras;
+	}
+
+	public int getHoras() {
+		return horas;
+	}
+
+	public void setHoras(int horas) {
+		this.horas = horas;
+	}
+	
+	
+}
--- treino_comp01/src/entidades/enums/NivelTrabalhador.java
+++ treino_comp01/src/entidades/enums/NivelTrabalhador.java
@@ -12,5 +12,4 @@ public enum NivelTrabalhador {
 	JUNIOR,
 	NIVEL_MEDIO,
 	SENIOR;
-
 }
