e9a28d836d83af437f63ec28f1d2020e75ac38c1
leo_dias <leo_dias@DESKTOP-FE887VA>
2021-08-11 20:16:06 -0300

    Criando classe Departamento

---
+++ treino_comp01/src/entidades/Departamento.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package entidades;
+
+/**
+ * @author leo_dias
+ *
+ */
+public class Departamento {
+	
+	private String nome;
+	
+	public Departamento() {
+	}
+	
+	public Departamento(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
